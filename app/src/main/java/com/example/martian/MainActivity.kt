861038f214083759de0648a07b1806ef3292e9e1
sid <shah.sgsits@gmail.com>
2021-12-20 07:59:16 +0530

    UI changes for listing

--- app/src/main/java/com/example/martian/MainActivity.kt
+++ app/src/main/java/com/example/martian/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.martian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.martian.photos.ui.MarsPhotosListActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, MarsPhotosListActivity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/example/martian/photos/ui/MarsPhotoListFragment.kt
+++ app/src/main/java/com/example/martian/photos/ui/MarsPhotoListFragment.kt
@@ -1,28 +1,123 @@
 package com.example.martian.photos.ui
 
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
 import com.example.martian.R
+import com.example.martian.databinding.FragmentMarsPhotoListBinding
+import com.example.martian.databinding.PhotoListItemBinding
+import com.example.martian.photos.PhotoListViewModel
+import com.example.martian.photos.model.Photo
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MarsPhotoListFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
+    private val photoListViewModel: PhotoListViewModel by viewModels()
+    private lateinit var binding: FragmentMarsPhotoListBinding
+    private var photosAdapter: PhotosAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_mars_photo_list, container, false)
+        binding = FragmentMarsPhotoListBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.photosList.layoutManager = GridLayoutManager(activity!!, 2)
+        photosAdapter = PhotosAdapter()
+        binding.photosList.adapter = photosAdapter
+
+        photoListViewModel.getPhotosByRoverName("curiosity")
+        photoListViewModel.getLoading().observe(this) { loading ->
+            showLoading(loading)
+        }
+        photoListViewModel.getError().observe(this) { error ->
+            showError(error)
+        }
+        photoListViewModel.getPhotos().observe(this) { photos ->
+            showData(photos)
+
+        }
+    }
+
+    private fun showData(photos: List<Photo>?) {
+        photos?.let {
+            photosAdapter?.addPhotos(photos)
+        }
+    }
+
+    private fun showError(error: Boolean) {
+        binding.errorState.root.visibility = if (error) View.VISIBLE else View.GONE
+
+    }
+
+    private fun showLoading(loading: Boolean) {
+        binding.loadingState.root.visibility = if (loading) View.VISIBLE else View.GONE
+    }
+
 
     companion object {
         @JvmStatic
         fun newInstance() =
             MarsPhotoListFragment().apply {
             }
+    }
+
+    class PhotosAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        private val photos = mutableListOf<Photo>()
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val binding =
+                PhotoListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return PhotoListItemViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            if (holder is PhotoListItemViewHolder) {
+                holder.bind(photos[position])
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return photos.size
+        }
+
+        fun addPhotos(photos: List<Photo>) {
+            this.photos.addAll(photos)
+            notifyDataSetChanged()
+        }
+
+        class PhotoListItemViewHolder(val binding: PhotoListItemBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+            fun bind(photo: Photo) {
+                binding.root.resources
+
+                binding.roverName.text = photo.rover.name
+                binding.cameraName.text = photo.camera.fullName
+                binding.photoDate.text = photo.earthDate
+                val applyDimension = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    200f,
+                    binding.root.resources.displayMetrics
+                )
+                Glide.with(binding.root).load(photo.imageSrc)
+                    .override(applyDimension.toInt(), applyDimension.toInt())
+                    .centerCrop()
+                    .into(binding.roverPhoto)
+
+            }
+
+        }
+
     }
 }
